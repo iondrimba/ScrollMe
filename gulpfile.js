fe04aa64f11a71a1b66baf736792a05c4e70ac2b
Ion D. Filho <iondrimba@gmail.com>
2016-03-14 17:53:11 -0300

    travis

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ gulp.task('browser-sync', require('./tasks/browser-sync.js'));
 gulp.task('coveralls', require('./tasks/coveralls.js'));
 
 // Default Task
-gulp.task('default', ['lint', 'copy', 'browser-sync','watch']);
+gulp.task('default', ['lint', 'copy', 'browser-sync', 'watch']);
 
 // Publish Task
-gulp.task('deploy', ['lint', 'coveralls', 'uglify', 'optimize']);
+gulp.task('deploy', ['lint', 'copy', 'uglify', 'optimize']);
